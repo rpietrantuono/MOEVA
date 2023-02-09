c63f09f4819b3fa4d504af418984317608faa0e2
rpietrantuono <roberto.pietrantuono@gmail.com>
2023-02-09 23:34:09 +0100

    tumor gfci

---
+++ RESULTS/GB/TUMOR/FINAL/run.sh
@@ -0,0 +1,14 @@
+#cd FGES/
+#sh run_config.sh
+#cd ..
+#echo running GFCI...
+#cd GFCI
+#sh run_config.sh
+#cd ..
+echo running RFCI...
+cd RFCI
+sh run_config.sh
+cd ..
+echo running random...
+cd RANDOM
+sh run_config.sh
